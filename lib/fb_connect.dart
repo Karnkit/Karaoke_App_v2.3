bfe75c7d0a355f1e9cab218e571920eb26a90442
Karnkit <karnkit.c@gmail.com>
2022-11-26 19:50:50 +0700

    first

---
+++ lib/fb_connect.dart
@@ -0,0 +1,65 @@
+import 'package:firebase_database/firebase_database.dart';
+
+class fb_connect {
+
+  Future loadSong(List listsong, String songname) async {
+    List listsong = [];
+    DatabaseReference ref = FirebaseDatabase.instance.ref();
+    final snapshot = await ref.child("Songs").orderByChild("title").equalTo(songname).get();
+    if (snapshot.exists) {
+      for(final key in snapshot.children){
+        listsong.add(key.value);
+      }
+    }
+    Future.delayed(const Duration(seconds: 1), () => "1");
+    return listsong;
+  }
+
+  Future loadUser(String userUID) async {
+    List userData = [];
+    DatabaseReference ref = FirebaseDatabase.instance.ref();
+    final snapshot = await ref.child("Users/"+userUID).get();
+    if (snapshot.exists) {
+      userData.add(snapshot.value);
+    }
+    Future.delayed(const Duration(seconds: 1), () => "1");
+    return userData;
+  }
+
+  Future loadRanking() async {
+    List usersranking = [];
+    DatabaseReference ref = FirebaseDatabase.instance.ref();
+    final snapshot = await ref.child("Users").orderByChild("stars").get();
+    if (snapshot.exists) {
+      for(final key in snapshot.children){
+        usersranking.add(key.value);
+      }
+    }
+    Future.delayed(const Duration(seconds: 1), () => "1");
+    return usersranking;
+  }
+
+  Future fetchingSongList(String userName) async {
+    List listSong = [];
+    DatabaseReference ref = FirebaseDatabase.instance.ref();
+    final snapshot = await ref.child("Songs").orderByChild("userName").equalTo(userName).get();
+    if(snapshot.exists){
+      for(final key in snapshot.children){
+        listSong.add(key.value);
+      }
+    }
+    Future.delayed(const Duration(seconds: 1), () => "1");
+    return listSong;
+  }
+
+  void writeData() async {
+    DatabaseReference ref = FirebaseDatabase.instance.ref("Instruments/123");
+    await ref.set({
+      "name": "John",
+      "age": 18,
+      "address": {
+        "line1": "100 Mountain View"
+      }
+    });
+  }
+}
---
+++ lib/root_app.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:karaoke_real_one/pages/home/karaoke_home.dart';
+import 'package:karaoke_real_one/pages/ranking/karaoke_ranking.dart';
+import 'package:karaoke_real_one/pages/Screen-profile/Profile_Page.dart';
+import 'package:karaoke_real_one/fb_connect.dart';
+
+class RootApp extends StatefulWidget {
+  final List userData;
+  final List rankingData;
+
+  RootApp({Key? key, required this.userData, required this.rankingData})
+      : super(key: key);
+
+  @override
+  _RootAppState createState() => _RootAppState();
+}
+
+class _RootAppState extends State<RootApp> {
+  int activeTab = 0;
+
+  List userData = [];
+  List rankingData = [];
+  List userSongs = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      bottomNavigationBar: getFooter(),
+      body: getBody(),
+    );
+  }
+
+  Widget getBody() {
+    bool firstState = true;
+    if (firstState) {
+      setState(() {
+        userData = widget.userData;
+        rankingData = widget.rankingData;
+      });
+      firstState = false;
+    }
+    return IndexedStack(
+      index: activeTab,
+      children: [
+        HomePage(userData: userData),
+        KaraokeRanking(usersRanking: rankingData),
+        ProfilePage(userData: userData)
+      ],
+    );
+  }
+
+  Widget getFooter() {
+    List items = [
+      Icons.home,
+      Icons.emoji_events_rounded,
+      Icons.supervisor_account_sharp
+    ];
+    return Container(
+      height: 70,
+      decoration: BoxDecoration(color: Colors.black),
+      child: Padding(
+        padding: const EdgeInsets.only(left: 30, right: 30),
+        child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: List.generate(items.length, (index) {
+              return IconButton(
+                  icon: Icon(
+                    items[index],
+                    color: activeTab == index ? Colors.green : Colors.white,
+                  ),
+                  onPressed: () async {
+                    setState(() {
+                      activeTab = index;
+                    });
+                    if (index == 1) {
+                      rankingData = await fb_connect().loadRanking();
+                    } else if (index == 2) {
+                      // setState(() {
+                      //   userData = widget.userData;
+                      // });
+                      if (userData[0]['songs_count'] > 0) {
+                        userData[0]['songs'] = await fb_connect()
+                            .fetchingSongList(userData[0]['userName']);
+                      }
+                    }
+                  });
+            })),
+      ),
+    );
+  }
+}
